1aa4351a0a05e0cfe5edbc2c24a6a443d16d370b
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-01-08 14:56:18 +0530

    Add Custom color And boxShadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
+    colors: {
+      'neutral': '#858199'
+    },
+    boxShadow : {
+      'card': '0 2px 5px rgba(133,129,153,0.25),0 1px 0 #E3E3E3,0 0 1px rgba(133,129,153,.1)',
+    },
     extend: {},
   },
   plugins: [],
