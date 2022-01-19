3fbc8bc2d7644ff92ce3047e5a5468eaa49f1d71
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-01-19 22:08:17 +0530

    Center The Song List

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,7 +2,7 @@
 import { SongCard } from './SongCard';
 export const SongList = ({ songs }) => {
     return (
-        <div className="flex flex-col gap-2 mt-2">
+        <div className="flex flex-col gap-2 mt-2 items-center">
             {songs.map((song,pos) => {
                return <SongCard key={song.name} pos={pos+1} cover={song.cover} songName={song.name} songDuration={song.duration} artist={song.artist} />
             })}
