6eaec7f66f0e98913ad3c8aa820c61a118173653
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-01-19 21:37:55 +0530

    Add Secondary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,14 @@ module.exports = {
   theme: {
     extend: {
       
-    colors: {
-      'neutral': '#858199',
-      'custom-grey': '#F3F0F7',
-    },
-    boxShadow : {
-      'card': '0 2px 5px rgba(133,129,153,0.25),0 1px 0 #E3E3E3,0 0 1px rgba(133,129,153,.1)',
-    },
+      colors: {
+        'neutral': '#858199',
+        'custom-grey': '#F3F0F7',
+        'secondary': '#7E1BCC'
+      },
+      boxShadow : {
+        'card': '0 2px 5px rgba(133,129,153,0.25),0 1px 0 #E3E3E3,0 0 1px rgba(133,129,153,.1)',
+      },
     },
   },
   plugins: [],
