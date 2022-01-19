5d1dcabb5183ccb9f6a84e73070d5c64c55c66f0
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-01-19 21:37:39 +0530

    fix : can't use tailwind default color classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
+    extend: {
+      
     colors: {
       'neutral': '#858199',
       'custom-grey': '#F3F0F7',
@@ -8,7 +10,7 @@ module.exports = {
     boxShadow : {
       'card': '0 2px 5px rgba(133,129,153,0.25),0 1px 0 #E3E3E3,0 0 1px rgba(133,129,153,.1)',
     },
-    extend: {},
+    },
   },
   plugins: [],
 }
