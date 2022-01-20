32fb760fe02c63d4bbf455ee4e41330533cfab0c
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-01-20 14:53:31 +0530

    Update Player Component To Play And Pause Song

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,29 @@
-export const Player = () => {
+
+import playIcon from "../assets/play.svg"
+import pauseIcon from "../assets/pause.svg"
+
+export const Player = ({ currentSong, audioRef, isPlaying, setIsPlaying }) => {
+
+    function togglePlay(e) {
+        if (isPlaying === true) {
+            setIsPlaying(false);
+            audioRef.current.pause();
+        }
+        else {
+            setIsPlaying(true);
+            audioRef.current.play();
+        }
+    }
     return (
-        <div className="w-full h-full bg-pink-400 z-50 ">
+        <div className="flex flex-col w-full h-full bg-[#FCFCFC] z-50 items-center justify-center">
+            <audio ref={audioRef}>
+                <source src={currentSong.url} type="audio/mpeg" />
+            </audio>
+
+            {isPlaying ?
+                <img src={pauseIcon} alt="pause" onClick={togglePlay} className="w-14" /> :
+                <img src={playIcon} alt="play" onClick={togglePlay} className="w-14" />
+            }
         </div>
     )
 }
