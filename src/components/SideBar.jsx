79bcf827e14fe2b43002e99252f77590624b704c
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-01-29 23:28:35 +0530

    Add Discover Section In SideBar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,6 +1,18 @@
+const Header =() =>{
+    return (
+        <div className="bg-white h-14">
+
+        </div>
+    )
+}
 export const SideBar = () => {
     return (
-        <div className="w-full h-full bg-fuchsia-500 ">
+        <div className="w-full h-full bg-[#FCFCFC] ">
+            <Header></Header>
+            <h3 className="text-[#c2c0cc] font-black text-lg uppercase tracking-wider pl-8">Discover</h3>
+            <div className="text-[#6a677a] font-medium text-sm pl-12">Feed</div>
+            <div className="text-[#6a677a] font-medium text-sm pl-12">Trending</div>
+            <div className="text-[#6a677a] font-medium text-sm pl-12">Explore</div>
         </div>
     )
 }
