16572952a8649e8dc54fa7553583f6c73976740c
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-02-01 15:38:16 +0530

    Add Library and Playlists Section to SideBar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,11 @@
-const Header =() =>{
+const Header = () => {
     return (
         <div className="bg-white h-14">
 
         </div>
     )
 }
+
 const UserCard = () => {
     return (
         <div className="flex flex-row pl-8 my-4">
@@ -17,14 +18,36 @@ const UserCard = () => {
     )
 }
 
-export const SideBar = () => {
+export const SideBar = ({ art }) => {
+
     return (
         <div className="w-full h-full bg-[#FCFCFC] ">
             <Header></Header>
+
+            <div className="h-[320px] overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-400 scroll-smooth">
+
+                <UserCard />
+
+                <div className="mb-4">
             <h3 className="text-[#c2c0cc] font-black text-lg uppercase tracking-wider pl-8">Discover</h3>
             <div className="text-[#6a677a] font-medium text-sm pl-12">Feed</div>
             <div className="text-[#6a677a] font-medium text-sm pl-12">Trending</div>
             <div className="text-[#6a677a] font-medium text-sm pl-12">Explore</div>
+                </div>
+
+                <div className="mb-4">
+                    <h3 className="text-[#c2c0cc] font-black text-lg uppercase tracking-wider pl-8">Library</h3>
+                    <div className="text-[#6a677a] font-medium text-sm pl-12">Favorites</div>
+                    <div className="text-[#6a677a] font-medium text-sm pl-12">History</div>
+                    <div className="text-[#6a677a] font-medium text-sm pl-12">Explore</div>
+                </div>
+
+                <div className="mb-4">
+                    <h3 className="text-[#c2c0cc] font-black text-lg uppercase tracking-wider pl-8">Playlists</h3>
+                </div>
+
+            </div>
+
         </div>
     )
 }
