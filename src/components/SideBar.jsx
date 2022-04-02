fa75a971424511d5d4fc6efbdd4086c760932cfd
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-04-02 20:31:39 +0530

    Refactor : Formatting

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -29,10 +29,10 @@ export const SideBar = ({ art }) => {
                 <UserCard />
 
                 <div className="mb-4">
-            <h3 className="text-[#c2c0cc] font-black text-lg uppercase tracking-wider pl-8">Discover</h3>
-            <div className="text-[#6a677a] font-medium text-sm pl-12">Feed</div>
-            <div className="text-[#6a677a] font-medium text-sm pl-12">Trending</div>
-            <div className="text-[#6a677a] font-medium text-sm pl-12">Explore</div>
+                    <h3 className="text-[#c2c0cc] font-black text-lg uppercase tracking-wider pl-8">Discover</h3>
+                    <div className="text-[#6a677a] font-medium text-sm pl-12">Feed</div>
+                    <div className="text-[#6a677a] font-medium text-sm pl-12">Trending</div>
+                    <div className="text-[#6a677a] font-medium text-sm pl-12">Explore</div>
                 </div>
 
                 <div className="mb-4">
