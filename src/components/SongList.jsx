fd25c6ef44be5259ad60f0e888c67bf4f308ffe0
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-04-22 13:27:01 +0530

    Change SongCard Key from id to TrackTitle

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -4,7 +4,7 @@ export const SongList = ({ songs, currentSong, handleSongChange }) => {
     return (
         <div className="flex flex-col gap-2 mt-2 items-center">
             {songs.map((song, pos) => {
-                return <SongCard key={song._id} pos={pos + 1} song={song} handleSongChange={handleSongChange} isActive={currentSong.TrackTitle === song.TrackTitle}/>
+                return <SongCard key={song.TrackTitle} pos={pos + 1} song={song} handleSongChange={handleSongChange} isActive={currentSong.TrackTitle === song.TrackTitle}/>
             })}
         </div>
     )
