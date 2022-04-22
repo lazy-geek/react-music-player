2ba37826785be009d310e4a89071434147b01fd4
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-04-22 11:58:32 +0530

    Add Upload Page & Fetch NewlyReleased Songs From Blockchain

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import React, { useState, useRef, useEffect } from "react";
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
+import {ethers} from 'ethers'
+import DMI_Contract_abi from './contracts/DMI-Contract_abi.json';
 import { SongList } from './components/SongList';
 import { Header } from './components/Header';
 import { SideBar } from './components/SideBar';
@@ -12,6 +20,94 @@ import { Triangle } from 'react-loader-spinner';
 
 
 function App() {
+  // -----------------------------------------------------------------------------------------------------------
+  let contractAddress = '0x152aE2599a1f85FcC9CFb5ed5cC47402004d050C';
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [connButtonText, setConnButtonText] = useState('Login');
+
+  const [currentContractVal, setCurrentContractVal] = useState(null);
+
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+  const [contract, setContract] = useState(null);
+
+  const connectWalletHandler = () => {
+    console.log("call");
+    if (window.ethereum && window.ethereum.isMetaMask) {
+
+      window.ethereum.request({ method: 'eth_requestAccounts' })
+        .then(result => {
+          console.log(result);
+          accountChangedHandler(result[0]);
+          setConnButtonText('Connected');
+        })
+        .catch(error => {
+          setErrorMessage(error.message);
+          console.log(error)
+        });
+
+    } else {
+      console.log('Need to install MetaMask');
+      setErrorMessage('Please install MetaMask browser extension to interact');
+    }
+  }
+
+  // update account, will cause component re-render
+  const accountChangedHandler = (newAccount) => {
+    setDefaultAccount(newAccount);
+    updateEthers();
+  }
+
+  const chainChangedHandler = () => {
+    // reload the page to avoid any errors with chain change mid use of application
+    window.location.reload();
+  }
+
+
+  // listen for account changes
+  window.ethereum.on('accountsChanged', accountChangedHandler);
+
+  window.ethereum.on('chainChanged', chainChangedHandler);
+
+  const updateEthers = () => {
+    console.log('contrxctr');
+    let tempProvider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(tempProvider);
+
+    let tempSigner = tempProvider.getSigner();
+    setSigner(tempSigner);
+
+    let tempContract = new ethers.Contract(contractAddress, DMI_Contract_abi, tempSigner);
+    setContract(tempContract);
+  }
+// ---------------------------------------------------------------------------------
+const fetchSongs = async ()  => {
+  if (contract === null) return;
+  let id =await contract.NewlyRealsed();
+  id = parseInt(id.toHexString(),16);
+  let songs=[];
+  for(let i=0; i<id; i++){
+    let song = await contract.getSongById(i);
+    let temp = {
+      'id':parseInt(song['_id'].toHexString(),16),
+      'TrackURL': song['ArtURL'],
+      "TrackTitle": song['SongName'],
+      "ReleaseDate": parseInt(song['TimeStamp'].toHexString(),16),
+      "ArtWorkURl": song['CoverURL'],
+      "TrackLikes": 0,
+      "TrackDuration": song['Length']
+    }
+    songs[i]=temp;
+  }
+  setSongList(songs);
+  console.log(songs);
+}
+
+useEffect(()=>{
+  fetchSongs();
+},[contract]);
+  // ---------------------------------------------------------------------------------
   // Ref
   const audioRef = useRef(null);
 
@@ -52,27 +148,37 @@ function App() {
   }, []);
 
   return (
-    // <div className="bg-custom-grey grid grid-cols-[20%_auto] grid-rows-[auto_auto_10%] h-screen overflow-hidden">
-    //   <div className="col-start-1 col-end-2 row-start-1 row-end-3">
-    //     <SideBar art={currentSong.ArtWorkURl}/>
-    //   </div>
-    //   <div className="col-start-2 col-end-3 row-start-1 row-end-2">
-    //     <Header title="Trending" />
-    //   </div>
-    //   <div className="col-start-2 col-end-3 row-start-2 row-end-3 overflow-y-scroll overflow-x-hidden">
-    //     <SongList songs={songList} currentSong={currentSong} handleSongChange={handleSongChange} />
-    //   </div>
-    //   <div className="col-start-1 col-end-3 row-start-3 row-end-4 ">
-    //     <Player currentSong={currentSong} audioRef={audioRef} isPlaying={isPlaying} setIsPlaying={setIsPlaying} songInfo={songInfo} setSongInfo={setSongInfo}/>
 
-    //   </div>
-    // </div>
 
-    <LoadingOverlay active={isUploading} spinner={<Triangle ariaLabel="loading-indicator" />}>
-      <div className="flex flex-col bg-custom-grey h-screen max-h-full items-center justify-center">
-        <UploadPage setIsUploading={setIsUploading} />
-      </div>
-    </LoadingOverlay>
+    <Routes>
+      <Route path="/" element={
+        <div className="bg-custom-grey grid grid-cols-[20%_auto] grid-rows-[auto_auto_10%] h-screen overflow-hidden">
+          <div className="col-start-1 col-end-2 row-start-1 row-end-3">
+            <SideBar art={currentSong.ArtWorkURl} connectWalletHandler={connectWalletHandler} connButtonText={connButtonText}/>
+          </div>
+          <div className="col-start-2 col-end-3 row-start-1 row-end-2">
+            <Header title="Trending" contract={contract} setSongList={setSongList} connectWalletHandler={connectWalletHandler} songList={songList}/>
+          </div>
+          <div className="col-start-2 col-end-3 row-start-2 row-end-3 overflow-y-scroll overflow-x-hidden">
+            <SongList songs={songList} currentSong={currentSong} handleSongChange={handleSongChange} />
+          </div>
+          <div className="col-start-1 col-end-3 row-start-3 row-end-4 ">
+            <Player currentSong={currentSong} audioRef={audioRef} isPlaying={isPlaying} setIsPlaying={setIsPlaying} songInfo={songInfo} setSongInfo={setSongInfo} />
+
+          </div>
+        </div>
+      } />
+      <Route path="upload" element={
+        <LoadingOverlay active={isUploading} spinner={<Triangle ariaLabel="loading-indicator" />}>
+          <div className="flex flex-col bg-custom-grey h-screen max-h-full items-center justify-center">
+            {/* {console.log('up: '+contract)} */}
+            <UploadPage setIsUploading={setIsUploading} contract={contract} connectWalletHandler={connectWalletHandler}/>
+          </div>
+        </LoadingOverlay>
+      } />
+
+    </Routes>
+
   );
 }
 
--- src/components/UploadPage.jsx
+++ src/components/UploadPage.jsx
@@ -3,6 +3,10 @@ import { Web3Storage } from 'web3.storage';
 import { getTime } from '../helpers';
 
 export const UploadPage = (props) => {
+    
+    
+
+	
     const [selectedSongFile, setSelectedSongFile] = useState();
     const [selectedCoverFile, setSelectedCoverFile] = useState();
     const [songName,setSongName] = useState("");
@@ -11,10 +15,13 @@ export const UploadPage = (props) => {
     const [quantity,setQuantity] = useState();
     const [preview, setPreview] = useState();
     const [src, setSrc] = useState("");
-    const [duration, setDuration] = useState("");
+    const [duration, setDuration] = useState();
     const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDU2MTY1MzdmYkYyQTdFODI3YzJkNzc5NzVGMzU1RTg3NTJDMTE5NzEiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDc3MDA0MjI2OTQsIm5hbWUiOiJyZWFjdC1tdXNpYy10ZXN0In0.jNBkj3OArl-n_c5smjsme-p2QgxQbyYGhGrdmzCSbTM";
     const storage = new Web3Storage({ token });
     
+    useEffect(() =>{
+        props.connectWalletHandler();
+    },[]);
     useEffect(() => {
         if (!selectedCoverFile) {
             setPreview(undefined)
@@ -46,10 +53,10 @@ export const UploadPage = (props) => {
         console.log(data);
         if (type === 'song') {
             setSelectedSongFile(data);
-            setIsSongFilePicked(true);
+            // setIsSongFilePicked(true);
         } else {
             setSelectedCoverFile(data);
-            setIsCoverFilePicked(true);
+            // setIsCoverFilePicked(true);
         }
 
 
@@ -76,17 +83,17 @@ export const UploadPage = (props) => {
         console.log(duration);
         const song_cid = await storage.put([newSongFile]);
         const cover_cid = await storage.put([newCoverFile]);
-        // await uploadSongDetails(newSongFile.name,song_cid,newCoverFile.name,cover_cid);
+        await uploadSongDetails(newSongFile.name,song_cid,newCoverFile.name,cover_cid);
         props.setIsUploading(false);
         console.log('Content added with CID:', song_cid);
         console.log('Content added with CID:', cover_cid);
     };
 
-    const uploadSongDetails = (songFile_name,song_cid,coverFile_name,cover_cid) => {
-      const song_url = `https://${song_cid}.ipfs.astyanax.io/${songFile_name}`;
-      const cover_url = `https://${cover_cid}.ipfs.astyanax.io/${coverFile_name}`;
-      
-        
+    const uploadSongDetails = async (songFile_name,song_cid,coverFile_name,cover_cid) => {
+      const song_url = `https://${song_cid}.ipfs.infura-ipfs.io/${songFile_name}`;
+      const cover_url = `https://${cover_cid}.ipfs.infura-ipfs.io/${coverFile_name}`;
+      const result = await props.contract.UploadSong(artistName,songName,cover_url,song_url,111,duration,'pop',price,quantity);
+        console.log(result);
     }
     const getFileExt = (f) => {
         return f.name.split('.').slice(-1)[0];
@@ -99,7 +106,7 @@ export const UploadPage = (props) => {
     }
     return (
         <div className=" flex flex-col border-[#dee2e6] border-[1px] max-w-lg px-16 py-12 bg-white  w-full">
-            <audio src={src} onLoadedMetadata={(e) => setDuration(getTime(e.target.duration))}>
+            <audio src={src} onLoadedMetadata={(e) => setDuration(getTime(parseInt(e.target.duration)))}>
                 
             </audio>
             <h1 className="font-bold text-4xl text-center mb-12">Upload Song</h1>
@@ -129,6 +136,9 @@ export const UploadPage = (props) => {
             <input type="file" name="cover" id='cover' onChange={(e) => changeHandler(e, 'cover')} accept="image/png,image/jpeg" hidden />
 
             <button onClick={handleSubmission} className="bg-secondary p-4 text-xl text-white">Submit</button>
+            
         </div>
     )
 };
+
+// https://bafybeiciu7fpruozdjusjtha63dyjpfb2jtnvjragyic36a2dkvpnqbpva.ipfs.infura-ipfs.io/Wiz_Khalifa.See_you_Again.jpg
