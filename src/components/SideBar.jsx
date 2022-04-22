cd879a6f385740484e09f0974681cf27644029cc
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-04-22 13:24:50 +0530

    Change 'Trending' to 'Newly Released'

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -30,7 +30,7 @@ export const SideBar = (props) => {
 
                 <div className="mb-4">
                     <h3 className="text-[#c2c0cc] font-black text-lg uppercase tracking-wider pl-8">Discover</h3>
-                    <div className="text-[#6a677a] font-medium text-sm pl-12">Feed</div>
+                    <div className="text-[#6a677a] font-medium text-sm pl-12">Newly Released</div>
                     <div className="text-[#6a677a] font-medium text-sm pl-12">Trending</div>
                     <div className="text-[#6a677a] font-medium text-sm pl-12">Explore</div>
                 </div>
