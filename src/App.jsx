3fa7c907700b228b14d20e62ee4cafdc2770f594
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-04-25 18:23:40 +0530

    Update contract Address

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const serverUrl = "https://dml-server.herokuapp.com";
 
 function App() {
   // -----------------------------------------------------------------------------------------------------------
-  let contractAddress = '0xa9570e99469A835DB159EA3192cB1473e06C58b1';
+  let contractAddress = '0x32713D92CC73c44BBC59A168DCC7CFDC865Df5e1';
   const [errorMessage, setErrorMessage] = useState(null);
   const [defaultAccount, setDefaultAccount] = useState(null);
   const [jwtToken, setJwtToken] = useState("");
