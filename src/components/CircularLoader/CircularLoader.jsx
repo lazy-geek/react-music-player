380fbe4f13e1304b3f3c62d455258a122a71df98
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-04-27 21:54:10 +0530

    Create CircularLoader And Checkmarks

---
+++ src/components/CircularLoader/CircularLoader.jsx
@@ -0,0 +1,36 @@
+
+import './CircularLoader.css';
+
+export const CircularLoader = (props) => {
+
+
+    return (
+        <div className="eEYUfV">
+
+            <div className="ieSfBq geucAj rounded-[32px] border-[3px]" style={{width: "32px", height: "32px",}}></div> 
+        </div>
+    )
+};
+
+export const GreyCheckMark = (props) => {
+
+
+    return (
+        <div className="eEYUfV">
+
+<svg viewBox="0 0 14 11" fill="none" width="30" height="30" xlmns="http://www.w3.org/2000/svg" ><path d="M1 5L5 9L13 1" className="stroke-[#6e6e6e]" strokeWidth="2" strokeLinecap="round"></path></svg>
+            </div>
+    )
+};
+
+export const BlueCheckMark = (props) => {
+
+
+    return (
+        <div className="eEYUfV">
+
+<svg viewBox="0 0 14 11" fill="none" width="30" height="30" xlmns="http://www.w3.org/2000/svg" ><path d="M1 5L5 9L13 1" className="stroke-[#0066ff]" strokeWidth="2" strokeLinecap="round"></path></svg>
+            </div>
+    )
+};
+// https://bafybeiciu7fpruozdjusjtha63dyjpfb2jtnvjragyic36a2dkvpnqbpva.ipfs.infura-ipfs.io/Wiz_Khalifa.See_you_Again.jpg
